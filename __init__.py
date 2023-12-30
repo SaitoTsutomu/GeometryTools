9d37bbf40db293708ac69e8305e9771f92bb0f55
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2023-12-30 12:47:09 +0900

    ver:for 4.0.0

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@ from .register_class import register, ui_classes, unregister  # noqa: F401
 bl_info = {
     "name": "GeometryTools",
     "author": "tsutomu",
-    "version": (0, 1),
-    "blender": (3, 4, 0),
+    "version": (0, 2),
+    "blender": (4, 0, 0),
     "support": "TESTING",
     "category": "Node",
     "description": "",
